01745939f033b2894a10a394fa19d2a7d5bab6de
Shane Fleming <sfleming@xilinx.com>
2024-12-03 11:30:27 +0000

    Registering Shuffle_hls

--- src/finnbrainsmith/custom_op/fpgadataflow/hls/__init__.py
+++ src/finnbrainsmith/custom_op/fpgadataflow/hls/__init__.py
@@ -1,7 +1,9 @@
 from finnbrainsmith.custom_op.fpgadataflow.hls.quantsoftmax_hls import QuantSoftmax_hls
+from finnbrainsmith.custom_op.fpgadataflow.hls.shuffle_hls import Shuffle_hls
 
 custom_op = dict()
 
 # make sure new HLSCustomOp subclasses are imported here so that they get
 # registered and plug in correctly into the infrastructure
 custom_op["QuantSoftmax_hls"] = QuantSoftmax_hls
+custom_op["Shuffle_hls"] = Shuffle_hls
