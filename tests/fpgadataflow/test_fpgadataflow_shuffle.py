d63b182ce30d00c81ea908dfb0bde318a8a565c0
Shane Fleming <sfleming@xilinx.com>
2024-12-06 10:34:40 +0000

    cppsim from pytorch to hw is passing

--- tests/fpgadataflow/test_fpgadataflow_shuffle.py
+++ tests/fpgadataflow/test_fpgadataflow_shuffle.py
@@ -4,6 +4,7 @@ import torch.onnx
 from torch import nn
 import onnx
 import tempfile
+import numpy as np
 
 from qonnx.core.datatype import DataType
 from qonnx.util.basic import gen_finn_dt_tensor, qonnx_make_model
@@ -86,7 +87,7 @@ def construct_onnx_model(
 
 
 @pytest.mark.fpgadataflow
-def test_pytorch_to_specialised():
+def test_pytorch_to_ip_gen():
     model = construct_onnx_model(
                 input_shape=(1,128,384),
                 transpose_perm=(0,2,1,3),
@@ -106,71 +107,6 @@ def test_pytorch_to_specialised():
     model = model.transform(CreateStitchedIP(test_fpga_part, test_synth_clk_period_ns))
 
 
-def make_single_shuffle_modelwrapper(
-        simd:int=1,
-        data_type=DataType["UINT8"],
-        in_reshaped:tuple[int]=(1, 128, 12, 32),
-        in_shape:tuple[int]=(1, 128, 384),
-        inner_moves:bool=False,
-        loop_coeffs:tuple[int]=(49152, 49152, 4096, 32),
-        out_reshaped:tuple[int]=(1, 12, 128, 32),
-        out_shape:tuple[int]=(1, 12, 128, 32),
-        perm:tuple[int]=(0, 2, 1, 3),
-        name:str="Shuffle_0"
-        ):
-    ''' Create a single Shuffle node '''
-    inp = helper.make_tensor_value_info("global_in", TensorProto.FLOAT, list(in_shape))
-    outp = helper.make_tensor_value_info("global_out", TensorProto.FLOAT, list(out_reshaped))
-
-    new_node = helper.make_node(
-            "Shuffle",
-            ["global_in"],
-            ["global_out"],
-            domain="finnbrainsmith.custom_op.fpgadataflow",
-            backend="fpgadataflow",
-            name=name,
-            simd=simd,
-            data_type=data_type.name,
-            in_reshaped=in_reshaped,
-            in_shape=in_shape,
-            inner_moves=inner_moves,
-            loop_coeffs=loop_coeffs,
-            out_reshaped=out_reshaped,
-            out_shape=out_shape,
-            perm=perm
-    )
-
-    graph = helper.make_graph(
-            [new_node],
-            "shuffle_graph",
-            inputs=[inp],
-            outputs=[outp]
-    )
-
-    model = qonnx_make_model(graph)
-    model = ModelWrapper(model)
-
-    model.set_tensor_datatype("global_in", data_type)
-    model.set_tensor_datatype("global_out", data_type)
-
-    return model
-
-
-@pytest.mark.fpgadataflow
-def test_convert_to_hw_shuffle_layer():
-    ''' Checks the conversion of a shuffle layer into hardware '''
-    model = make_single_shuffle_modelwrapper() 
-
-    # Attempt to build the HLS for this
-    model = model.transform(SpecializeLayers(test_fpga_part))
-    model = model.transform(GiveUniqueNodeNames())
-    model = model.transform(GiveReadableTensorNames())
-
-    model = model.transform(PrepareIP(test_fpga_part, test_synth_clk_period_ns))
-    model = model.transform(HLSSynthIP())
-    model = model.transform(CreateStitchedIP(test_fpga_part, test_synth_clk_period_ns))
-    
-
 @pytest.mark.parametrize("in_shape", [(1, 128, 384)])
 @pytest.mark.parametrize("in_reshaped", [(1, 128, 12, 32)])
 @pytest.mark.parametrize("out_shape", [(1, 12, 128, 32)])
@@ -184,22 +120,21 @@ def test_cppsim_shuffle_layer(in_shape, in_reshaped, out_shape, out_reshaped, pe
     dt = DataType[datatype]
     simd = int(simd[-1])
 
-    model = make_single_shuffle_modelwrapper(
-                simd=simd,
-                data_type=dt,
-                in_reshaped=in_reshaped,
-                in_shape=in_shape,
-                inner_moves=False,
-                loop_coeffs=shuffle_perfect_loopnest_coeffs(in_reshaped,perm),
-                out_reshaped=out_reshaped,
-                out_shape=out_shape,
-                perm=perm
-            )
+    model = construct_onnx_model(
+            input_shape=in_shape,
+            transpose_perm=perm,
+            reshape1_shape=in_reshaped,
+            reshape2_shape=out_reshaped,
+            dt=dt
+    )
 
     input = gen_finn_dt_tensor(dt, in_shape)
     in_name = model.graph.input[0].name
     out_name = model.graph.output[0].name
     input_t = {in_name : input}
+
+    # Get a reference for the shuffle 
+    y_ref = oxe.execute_onnx(model, input_t)[out_name]
 
     # Attempt to build the HLS for this
     model = model.transform(SpecializeLayers(test_fpga_part))
@@ -212,5 +147,12 @@ def test_cppsim_shuffle_layer(in_shape, in_reshaped, out_shape, out_reshaped, pe
 
     y_hw = oxe.execute_onnx(model, input_t)[out_name]
 
+    y_hw_flat = y_hw.flatten()
+    y_ref_flat = y_ref.flatten()
+    for i in range(len(y_hw_flat)):
+        if not np.allclose(y_hw_flat[i], y_ref_flat[i]):
+            print(f"Index {i}, Expected {y_ref_flat[i]} -- Got {y_hw_flat[i]}")
+
+    assert np.allclose(y_ref, y_hw), "Model output does not match expected output"
     
 
