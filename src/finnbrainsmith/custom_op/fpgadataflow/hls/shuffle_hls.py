c50ef9868365d6a48da46f0e08c5e83c8afb6faf
Shane Fleming <sfleming@xilinx.com>
2024-12-19 11:50:59 +0000

    simd to SIMD

--- src/finnbrainsmith/custom_op/fpgadataflow/hls/shuffle_hls.py
+++ src/finnbrainsmith/custom_op/fpgadataflow/hls/shuffle_hls.py
@@ -63,7 +63,7 @@ class Shuffle_hls(Shuffle, BS_HLSBackend):
 
     def get_exp_cycles(self):
         out_shape = self.get_nodeattr("out_shape")
-        simd = self.get_nodeattr("simd")
+        simd = self.get_nodeattr("SIMD")
         return np.prod(out_shape)/simd 
 
     def docompute(self):
