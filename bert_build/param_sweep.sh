2e7ab18d211f4d893a90ff09f30cc7561faaa2fe
Shane Fleming <sfleming@xilinx.com>
2025-01-21 20:10:54 +0000

    Slight tweak to example param sweep script

--- bert_build/param_sweep.sh
+++ bert_build/param_sweep.sh
@@ -1,12 +1,12 @@
 #!/bin/bash  
   
-for heads in 12 24 36; do  
+for fps in 1000 2000 3000; do  
+  for heads in 12 24 36; do  
     for hidden_size in 384 192 96; do  
-        for bitwidth in 8 4; do  
-            for fps in 1000 2000 3000; do  
-                python endtoend.py -o ${heads}_${hidden_size}_${bitwidth}_${fps}.onnx -s step_hw_ipgen -n $heads -z $hidden_size -b $bitwidth -f $fps 
+        #for bitwidth in 8 4; do  
+                python endtoend.py -o ${heads}_${hidden_size}_${bitwidth}_${fps}.onnx -s step_hw_ipgen -n $heads -z $hidden_size -f $fps 
                 mv intermediate_models ${heads}_${hidden_size}_${bitwidth}_${fps}  
             done  
-        done  
+        #done  
     done  
 done  
