ff8f766aab622f565a3b25c06a56fb011bcbd448
Shane Fleming <sfleming@xilinx.com>
2025-01-21 15:28:41 +0000

    fix param sweep

--- bert_build/param_sweep.sh
+++ bert_build/param_sweep.sh
@@ -4,7 +4,7 @@ for heads in 12 24 36; do
     for hidden_size in 384 192 96; do  
         for bitwidth in 8 4; do  
             for fps in 1000 2000 3000; do  
-                python endtoend.py -o ${heads}_${hidden_size}_${bitwidth}_${fps}.onnx -s step_hw_ipgen 
+                python endtoend.py -o ${heads}_${hidden_size}_${bitwidth}_${fps}.onnx -s step_hw_ipgen -n $heads -z $hidden_size -b $bitwidth -f $fps 
                 mv intermediate_models ${heads}_${hidden_size}_${bitwidth}_${fps}  
             done  
         done  
