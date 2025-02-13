e5cb7ed254ed2f5a20acd461ce0793be682a72b8
Thomas Keller <thomaskeller@microsoft.com>
2025-02-13 19:18:29 +0000

    Initial FINN docker transplant

---
+++ fetch-repos.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+CNPY_COMMIT="8c82362372ce600bbd1cf11d64661ab69d38d7de"
+HLSLIB_COMMIT="7783acaac835e702da25aa6b7103254b3cbcdf83"
+OMX_COMMIT="0b59762f9e4c4f7e5aa535ee9bc29f292434ca7a"
+AVNET_BDF_COMMIT="2d49cfc25766f07792c0b314489f21fe916b639b"
+XIL_BDF_COMMIT="8cf4bb674a919ac34e3d99d8d71a9e60af93d14e"
+RFSOC4x2_BDF_COMMIT="13fb6f6c02c7dfd7e4b336b18b959ad5115db696"
+KV260_BDF_COMMIT="98e0d3efc901f0b974006bc4370c2a7ad8856c79"
+EXP_BOARD_FILES_MD5="226ca927a16ea4ce579f1332675e9e9a"
+PYXSI_COMMIT="4f4ec10a3631c4c44b5bc0ede698d41c924d2b86"
+
+CNPY_URL="https://github.com/maltanar/cnpy.git"
+HLSLIB_URL="https://github.com/Xilinx/finn-hlslib.git"
+OMX_URL="https://github.com/maltanar/oh-my-xilinx.git"
+AVNET_BDF_URL="https://github.com/Avnet/bdf.git"
+XIL_BDF_URL="https://github.com/Xilinx/XilinxBoardStore.git"
+RFSOC4x2_BDF_URL="https://github.com/RealDigitalOrg/RFSoC4x2-BSP.git"
+KV260_BDF_URL="https://github.com/Xilinx/XilinxBoardStore.git"
+PYXSI_URL="https://github.com/maltanar/pyxsi.git"
+
+CNPY_DIR="cnpy"
+HLSLIB_DIR="finn-hlslib"
+OMX_DIR="oh-my-xilinx"
+AVNET_BDF_DIR="avnet-bdf"
+XIL_BDF_DIR="xil-bdf"
+RFSOC4x2_BDF_DIR="rfsoc4x2-bdf"
+KV260_SOM_BDF_DIR="kv260-som-bdf"
+PYXSI_DIR="pyxsi"
+
+# absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+fetch_repo() {
+    # URL for git repo to be cloned
+    REPO_URL=$1
+    # commit hash for repo
+    REPO_COMMIT=$2
+    # directory to clone to under deps/
+    REPO_DIR=$3
+    # absolute path for the repo local copy
+    CLONE_TO=$SCRIPTPATH/deps/$REPO_DIR
+
+    # clone repo if dir not found
+    if [ ! -d "$CLONE_TO" ]; then
+        git clone $REPO_URL $CLONE_TO
+    fi
+    # verify and try to pull repo if not at correct commit
+    CURRENT_COMMIT=$(git -C $CLONE_TO rev-parse HEAD)
+    if [ $CURRENT_COMMIT != $REPO_COMMIT ]; then
+        git -C $CLONE_TO pull
+        # checkout the expected commit
+        git -C $CLONE_TO checkout $REPO_COMMIT
+    fi
+    # verify one last time
+    CURRENT_COMMIT=$(git -C $CLONE_TO rev-parse HEAD)
+    if [ $CURRENT_COMMIT == $REPO_COMMIT ]; then
+        echo "Successfully checked out $REPO_DIR at commit $CURRENT_COMMIT"
+    else
+        echo "Could not check out $REPO_DIR. Check your internet connection and try again."
+    fi
+}
+
+fetch_board_files() {
+    echo "Downloading and extracting board files..."
+    mkdir -p "$SCRIPTPATH/deps/board_files"
+    OLD_PWD=$(pwd)
+    cd "$SCRIPTPATH/deps/board_files"
+    wget -q https://github.com/cathalmccabe/pynq-z1_board_files/raw/master/pynq-z1.zip
+    wget -q https://dpoauwgwqsy2x.cloudfront.net/Download/pynq-z2.zip
+    unzip -q pynq-z1.zip
+    unzip -q pynq-z2.zip
+    cp -r $SCRIPTPATH/deps/$AVNET_BDF_DIR/* $SCRIPTPATH/deps/board_files/
+    cp -r $SCRIPTPATH/deps/$XIL_BDF_DIR/boards/Xilinx/rfsoc2x2 $SCRIPTPATH/deps/board_files/;
+    cp -r $SCRIPTPATH/deps/$RFSOC4x2_BDF_DIR/board_files/rfsoc4x2 $SCRIPTPATH/deps/board_files/;
+    cp -r $SCRIPTPATH/deps/$KV260_SOM_BDF_DIR/boards/Xilinx/kv260_som $SCRIPTPATH/deps/board_files/;
+    cd $OLD_PWD
+}
+
+
+cat <(tail -n +2 python_repos.txt) | while IFS=',' read -a arr ; do
+    # extract line to $arr as array separated by ','
+    fetch_repo "${arr[1]}" "${arr[2]}" "${arr[0]}"
+done
+
+fetch_repo $CNPY_URL $CNPY_COMMIT $CNPY_DIR
+fetch_repo $HLSLIB_URL $HLSLIB_COMMIT $HLSLIB_DIR
+fetch_repo $OMX_URL $OMX_COMMIT $OMX_DIR
+fetch_repo $AVNET_BDF_URL $AVNET_BDF_COMMIT $AVNET_BDF_DIR
+fetch_repo $XIL_BDF_URL $XIL_BDF_COMMIT $XIL_BDF_DIR
+fetch_repo $RFSOC4x2_BDF_URL $RFSOC4x2_BDF_COMMIT $RFSOC4x2_BDF_DIR
+fetch_repo $KV260_BDF_URL $KV260_BDF_COMMIT $KV260_SOM_BDF_DIR
+fetch_repo $PYXSI_URL $PYXSI_COMMIT $PYXSI_DIR
+
+# Can skip downloading of board files entirely if desired
+if [ "$FINN_SKIP_BOARD_FILES" = "1" ]; then
+    echo "Skipping download and verification of board files"
+else
+    # download extra board files and extract if needed
+    if [ ! -d "$SCRIPTPATH/deps/board_files" ]; then
+        fetch_board_files
+    else
+        cd $SCRIPTPATH
+        BOARD_FILES_MD5=$(find deps/board_files/ -type f -exec md5sum {} \; | sort -k 2 | md5sum | cut -d' ' -f 1)
+        if [ "$BOARD_FILES_MD5" = "$EXP_BOARD_FILES_MD5" ]; then
+            echo "Verified board files folder content md5: $BOARD_FILES_MD5"
+        else
+            echo "Board files folder md5: expected $BOARD_FILES_MD5 found $EXP_BOARD_FILES_MD5"
+            echo "Board files folder content mismatch, removing and re-downloading"
+            rm -rf deps/board_files/
+            fetch_board_files
+        fi
+    fi
+fi
