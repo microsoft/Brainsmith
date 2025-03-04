6fae2140fa54fb6fd523720d071b6303afcd0bb6
Shane Fleming <sfleming@xilinx.com>
2025-03-04 17:03:18 +0000

    Moving the constraining of shuffle paramemters and pumpedCompute to temporary custom transformations so that they are more reliable

--- src/finnbrainsmith/util/bert.py
+++ src/finnbrainsmith/util/bert.py
@@ -10,6 +10,7 @@
 import onnx
 import argparse
 from onnxsim import simplify
+import qonnx.custom_op.registry as registry
 from qonnx.util.cleanup import cleanup
 from qonnx.transformation.general import (
         SortCommutativeInputsInitializerLast, 
@@ -254,6 +255,51 @@ def custom_step_cleanup(model, cfg):
     #)
     model = model.transform(SortCommutativeInputsInitializerLast())
     model = model.transform(RemoveIdentityOps())
+    return model
+
+class SetPumpedCompute(Transformation):
+    """ For all MVAUs and DynMatMuls set the pumped compute attribute """
+    def __init__(self):
+        super().__init__()
+
+    def apply(self, model):
+        graph = model.graph
+        graph_modified = False
+
+        for node in graph.node:
+            if (node.op_type == "MVAU_rtl"):
+                inst = registry.getCustomOp(node)
+                inst.set_nodeattr("pumpedCompute", 1)
+                graph_modified = True
+        return(model, graph_modified)
+
+
+class TempShuffleFixer(Transformation):
+    """ A temporary transformation that ensures that shuffles are sized correctly for the
+    initial BERT builds """
+
+    def __init__(self):
+        super().__init__()
+
+    def apply(self, model):
+        graph = model.graph
+        graph_modified = False
+
+        for node in graph.node:
+            if node.op_type == "Shuffle_hls":
+                inst = registry.getCustomOp(node)
+                inner_moves = inst.get_nodeattr("inner_moves")
+                simd = inst.get_nodeattr("SIMD")
+                if (inner_moves == 1) and (simd > 1):
+                    print(f"WARNING: as a safety precaution changing the shuffle where the inner dimension moves to SIMD=1 \n{node=}")
+                    inst.set_nodeattr("SIMD", 1)
+                    graph_modified = True
+        return (model, graph_modified)
+
+
+def custom_step_constrain_folding_and_set_pumped_compute(model, cfg):
+    model = model.transform(TempShuffleFixer())
+    model = model.transform(SetPumpedCompute())
     return model
 
 class QuantizeLayerNormalization(Transformation):
