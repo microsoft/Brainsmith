13b4b349f4fdabec002a0f812f1c20ba28a89cd5
tafk7 <tkeller787@gmail.com>
2025-04-03 23:12:04 +0000

    bert_steps path fix in tests

--- tests/fpgadataflow/test_bert_endtoend.py
+++ tests/fpgadataflow/test_bert_endtoend.py
@@ -31,7 +31,7 @@ from finn.transformation.fpgadataflow.prepare_rtlsim import PrepareRTLSim
 import finn.builder.build_dataflow_config as build_cfg
 import finn.core.onnx_exec as oxe
 
-from brainsmith.util.bert import (
+from brainsmith.jobs.bert.bert_steps import (
         custom_step_remove_head, 
         custom_step_remove_tail, 
         custom_step_cleanup,
