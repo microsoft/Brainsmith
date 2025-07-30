7e6c3ede69259e52e5ce7bc2960dedb54310f3c5
auphelia <jakobapk@web.de>
2025-07-30 11:58:06 +0100

    [Deps] Update brainsmith with recent finn changes

--- docker/fetch-repos.sh
+++ docker/fetch-repos.sh
@@ -15,8 +15,8 @@ recho() { echo -e "${RED}$1${NC}"; }
 yecho() { echo -e "${YELLOW}$1${NC}"; }
 
 # Dependency Git URLs, hashes/branches, and directory names
-QONNX_URL="https://github.com/tafk7/qonnx.git"
-FINN_URL="https://github.com/tafk7/finn.git"
+QONNX_URL="https://github.com/fastmachinelearning/qonnx.git"
+FINN_URL="https://github.com/Xilinx/finn.git"
 FINN_EXP_URL="https://github.com/Xilinx/finn-experimental.git"
 BREVITAS_URL="https://github.com/Xilinx/brevitas.git"
 CNPY_URL="https://github.com/maltanar/cnpy.git"
@@ -28,8 +28,8 @@ RFSOC4x2_BDF_URL="https://github.com/RealDigitalOrg/RFSoC4x2-BSP.git"
 KV260_BDF_URL="https://github.com/Xilinx/XilinxBoardStore.git"
 ONNXSCRIPT_URL="https://github.com/jsmonson/onnxscript.git"
 
-QONNX_COMMIT="custom/brainsmith-transform-registry"
-FINN_COMMIT="custom/brainsmith-patch"
+QONNX_COMMIT="custom/brainsmith"
+FINN_COMMIT="custom/transformer"
 FINN_EXP_COMMIT="0724be21111a21f0d81a072fccc1c446e053f851"
 BREVITAS_COMMIT="95edaa0bdc8e639e39b1164466278c59df4877be"
 CNPY_COMMIT="8c82362372ce600bbd1cf11d64661ab69d38d7de"
